14c15d1c1fbac729dfa8d2ffd70a014440f24ef0
Victor Alvarez <victoordanilo@gmail.com>
2024-10-09 16:21:15 -0500

    Navbar y sidebar creados

--- front-Drogueria/src/app/dashboard/dashboard.component.ts
+++ front-Drogueria/src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import NavbarComponent from '../shared/navbar/navbar.component';
+import SidebarComponent from '../shared/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, NavbarComponent, SidebarComponent],
   templateUrl: './dashboard.component.html',
-  styles: ``
+  styles: ``,
 })
-export default class DashboardComponent {
-
-}
+export default class DashboardComponent {}
---
+++ front-Drogueria/src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './sidebar.component.html',
+  styles: ``,
+})
+export default class SidebarComponent {}
